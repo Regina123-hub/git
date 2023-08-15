aff8ff18247bcc84adda851e45d109c8b49db982
Regina123-hub <reginka6964@gmail.com>
2023-08-15 22:40:49 +0400

    добавлено меню

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                1.Сложить 
+                2.Вычесть
+                """);
         System.out.println("Enter number: ");
         int a = scanner.nextInt();
         System.out.println("Enter number: ");
