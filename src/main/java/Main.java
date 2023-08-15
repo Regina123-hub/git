06897cfae52158c92fbe46336854ca390eee223e
Regina123-hub <reginka6964@gmail.com>
2023-08-15 22:52:21 +0400

    добавлен оператор switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("""
-                 1.Сложить 
-                 2.Вычесть
-                 3.Выход
-                """);
-        int input = scanner.nextInt();
+        int input = 0;
+
         while (input != 3) {
-            System.out.println("Enter number: ");
-            int a = scanner.nextInt();
-            System.out.println("Enter number: ");
-            int b = scanner.nextInt();
-            System.out.println(a + b);
+            System.out.println("""
+                     1.Сложить 
+                     2.Вычесть
+                     3.Выход
+                    """);
+            input = scanner.nextInt();
+
+            switch (input) {
+                case 1 -> {
+                    System.out.println("Enter number: ");
+                    int a = scanner.nextInt();
+                    System.out.println("Enter number: ");
+                    int b = scanner.nextInt();
+                    System.out.println(a + b);
+                }
+                case 2 -> {
+                    System.out.println("Enter number: ");
+                    int a = scanner.nextInt();
+                    System.out.println("Enter number: ");
+                    int b = scanner.nextInt();
+                    System.out.println(a - b);
+                }
+
+            }
+
         }
 
     }
